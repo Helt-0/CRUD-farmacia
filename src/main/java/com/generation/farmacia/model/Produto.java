0aae1161a6f64a5473519b6224e3d20c911ab3a0
lucas silva <lucas.vasconcelosop@hotmail.com>
2024-07-22 10:19:20 -0300

    :sparkles: feat: Implementando o CRUD da classe Produto.

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,89 @@
+package com.generation.farmacia.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@NotBlank(message = "O nome do produto é obrigatório.")
+	private String nome;
+	
+	@NotBlank(message = "A descrição do produto é obrigatória.")
+	@Size(max = 100, message = "A descrição deve ter no máximo 1000 caracteres.")
+	private String descricao;
+	
+	@Digits(integer= 8, fraction= 2)
+	@NotNull(message = "O preço do produto é obrigatorio.")
+	private BigDecimal preco;
+	
+	@NotNull(message = "A quantidade é obrigatória no cadastro.")
+	private Integer quantidade;
+	
+	@NotBlank(message = "O produto deve conter uma imagem ilustrativa.")
+	private String foto;
+	
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+}
