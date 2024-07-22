283e1e4aa8728e00f59feac85c12ff37bdbcac4c
lucas silva <lucas.vasconcelosop@hotmail.com>
2024-07-22 09:56:21 -0300

    :sparkles: feat: Implemetando o CRUD da classe Categoria.

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,52 @@
+package com.generation.farmacia.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@NotBlank(message = "O corredor deve conter um nome.")
+	private String nome;
+	
+	@NotBlank(message ="A descrição não pode ficar em branco.")
+	@Size(max = 100, message = "A descrição deve ter no máximo 1000 caracteres.")
+	private String descricao;
+	
+	
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
